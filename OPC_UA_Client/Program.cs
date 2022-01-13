f28d47e3778cc0859ae382b6153f9bc798416a0e
Lukas Hofer <92304563+automationTurtle95@users.noreply.github.com>
2022-01-13 12:39:52 +0100

    Projektdateien hinzufügen.

---
+++ OPC_UA_Client/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+namespace OPC_UA_Client // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static double a = 10;
+        public static double b = 20;
+
+        public static void Main(string[] args)
+        {
+            OpcNodeInfo info;
+
+            OpcValue value;
+
+            OpcStatus status;
+
+            object[] result;
+
+            Random random = new Random();
+
+            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            {
+                //Connect
+                client.Connect();
+
+                //Browse nodes
+                info = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                //Print(info);
+
+                //Read node
+                value = client.ReadNode("ns=2;s=Machine/Running");
+                Console.WriteLine($"[opc-ua-client] Read node = {value}");
+
+                //Read node attributes
+                value = client.ReadNode("ns=2;s=Machine/Running", OpcAttribute.DisplayName);
+                Console.WriteLine($"[opc-ua-client] Read node display name = {value}");
+
+                //Write node
+                status = client.WriteNode("ns=2;s=Machine/Parameter", 0);
+                Console.WriteLine($"[opc-ua-client] Write node = {status}");
+
+                //Write node attribute
+                status = client.WriteNode("ns=2;s=MAchine/Parameter", OpcAttribute.DisplayName, "Parameter");
+                Console.WriteLine($"[opc-ua-client] Write node display name = {status}");
+
+                //read/write node
+                /*for (var i = 0; i < 10; i++)
+                {
+                    value = client.ReadNode("ns=2;s=Machine/Parameter");
+                    Console.WriteLine($"[opc-ua-client] Read node = {value}");
+
+                    if (value.Status.IsGood && value.DataType == OpcDataType.Int32)
+                    {
+                        status = client.WriteNode("ns=2;s=Machine/Parameter", (int)value.Value+1);
+                        Console.WriteLine($"[opc-ua-client] Write node = {status}");
+                    }
+
+                }*/
+                Console.WriteLine();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    //Call methodA
+                    result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodA", a+i, b);
+                    Console.WriteLine($"[opc-ua-client] call methodA = {result[0]}");
+                    status = client.WriteNode("ns=2;s=Machine/methodACall", true);
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    ////Call methodB
+                    result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodB", a+i, b);
+                    Console.WriteLine($"[opc-ua-client] call methodB = {result[0]}");
+                    status = client.WriteNode("ns=2;s=Machine/methodBCall", true);
+                }
+
+                for (int i = 0; i < 7; i++)
+                {
+                    ////Call methodC
+                    result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodC", a, b+i);
+                    Console.WriteLine($"[opc-ua-client] call methodC = {result[0]}");
+                    status = client.WriteNode("ns=2;s=Machine/methodCCall", true);
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    //Call methodD
+                    result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodD", a, b+i);
+                    Console.WriteLine($"[opc-ua-client] call methodD = {result[0]}");
+                    status = client.WriteNode("ns=2;s=Machine/methodDCall", true);
+                }
+
+                int rnd = random.Next(20, 70);
+
+                for (int i = 0; i < rnd; i++)
+                {
+                    ////Call methodC
+                    result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodC", a, b+i);
+                    Console.WriteLine($"[opc-ua-client] call methodC = {result[0]}");
+                    status = client.WriteNode("ns=2;s=Machine/methodCCall", true);
+                }
+
+                value = client.ReadNode("ns=2;s=Machine/CallsMethodC");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Random number {rnd} --- Calls Method C {value}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                if (rnd == value)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Correct number of methodcalls!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("[opc-ua-client] Press enter to exit");
+
+
+        }
+
+
+
+        static void Print(OpcNodeInfo parent, int level = 0)
+        {
+            var space = new string(' ', level);
+            var id = parent.NodeId;
+            var name = parent.Attribute(OpcAttribute.DisplayName).Value;
+
+            Console.WriteLine($"[opc-ua-client]{space}{id} {name}");
+            foreach (var child in parent.Children())
+            {
+                Print(child, level+1);
+            }
+        }
+    }
+}
---
+++ OPC_UA_Server/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Opc.UaFx;
+using Opc.UaFx.Server;
+
+
+namespace OPC_UA_Server // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        private static Random rnd = new Random();
+
+        private static OpcFolderNode machine;
+        private static OpcDataVariableNode<bool> running;
+        private static OpcDataVariableNode<int> version;
+        private static OpcDataVariableNode<int> random;
+        private static OpcDataVariableNode<int> parameter;
+
+        private static OpcDataVariableNode<bool> methodACall;
+        private static OpcDataVariableNode<bool> methodBCall;
+        private static OpcDataVariableNode<bool> methodCCall;
+        private static OpcDataVariableNode<bool> methodDCall;
+
+        private static OpcDataVariableNode<int> nrMethodACalls;
+        private static OpcDataVariableNode<int> nrMethodBCalls;
+        private static OpcDataVariableNode<int> nrMethodCCalls;
+        private static OpcDataVariableNode<int> nrMethodDCalls;
+
+        private static OpcMethodNode methodA;
+        private static OpcMethodNode methodB;
+        private static OpcMethodNode methodC;
+        private static OpcMethodNode methodD;
+
+        private static OpcVariableValue<object> WriteParameter(OpcContext context, OpcVariableValue<object> value)
+        {
+            //Console.WriteLine($"New value {value.Value}");
+            return value;
+        }
+
+        private static double MethodA(double a, double b)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Called method A: add to numbers");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if(a.GetType() != typeof(double) || b.GetType() != typeof(double))
+            {
+                throw new OpcException();
+            }
+
+            return a+b;
+        }
+
+        private static double MethodB(double a, double b)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Called method B: subtract to numbers");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (a.GetType() != typeof(double) || b.GetType() != typeof(double))
+            {
+                throw new OpcException();
+            }
+            return a-b;
+        }
+
+        private static double MethodC(double a, double b)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Called method C: multiply to numbers");
+            Console.ForegroundColor = ConsoleColor.White;
+            if (a.GetType() != typeof(double) || b.GetType() != typeof(double))
+            {
+                throw new OpcException();
+            }
+            return a*b;
+        }
+
+        private static double MethodD(double a, double b)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Called method D: divide to numbers");
+            Console.ForegroundColor = ConsoleColor.White;
+            if (a.GetType() != typeof(double) || b.GetType() != typeof(double))
+            {
+                throw new OpcException();
+            }
+            return a/b;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            machine = new OpcFolderNode(new OpcName("Machine"));
+
+            running = new OpcDataVariableNode<bool>("Running", true); //Variablenknoten hinzufügen
+            version = new OpcDataVariableNode<int>("Version", 1);
+            random = new OpcDataVariableNode<int>("Random", 0);
+
+            nrMethodACalls = new OpcDataVariableNode<int>("CallsMethodA", 1);
+            nrMethodBCalls = new OpcDataVariableNode<int>("CallsMethodB", 1);
+            nrMethodCCalls = new OpcDataVariableNode<int>("CallsMethodC", 1);
+            nrMethodDCalls = new OpcDataVariableNode<int>("CallsMethodD", 1);
+
+            List<OpcDataVariableNode<int>> Calls = new List<OpcDataVariableNode<int>>();
+            Calls.Add(nrMethodACalls);
+            Calls.Add(nrMethodBCalls);
+            Calls.Add(nrMethodCCalls);
+            Calls.Add(nrMethodDCalls);
+
+            parameter = new OpcDataVariableNode<int>(machine, "Parameter", 0);  //variablenknoten hinzufügen mit Schreibfunktion
+            parameter.WriteVariableValueCallback = WriteParameter;
+
+            methodACall = new OpcDataVariableNode<bool>(machine, "methodACall", false);
+            methodACall.WriteVariableValueCallback = WriteParameter;
+
+            methodBCall = new OpcDataVariableNode<bool>(machine, "methodBCall", false);
+            methodBCall.WriteVariableValueCallback = WriteParameter;
+
+            methodCCall = new OpcDataVariableNode<bool>(machine, "methodCCall", false);
+            methodCCall.WriteVariableValueCallback = WriteParameter;
+
+            methodDCall = new OpcDataVariableNode<bool>(machine, "methodDCall", false);
+            methodDCall.WriteVariableValueCallback = WriteParameter;
+
+            List<OpcDataVariableNode<bool>> Called = new List<OpcDataVariableNode<bool>>();
+            Called.Add(methodACall);
+            Called.Add(methodBCall);
+            Called.Add(methodCCall);
+            Called.Add(methodDCall);
+
+            //####### Methodenknoten anlegen
+            methodA = new OpcMethodNode(machine, new OpcName("MethodA"), new Func<double, double, double>(MethodA));
+
+            methodB = new OpcMethodNode(machine, new OpcName("MethodB"), new Func<double, double, double>(MethodB));
+            methodC = new OpcMethodNode(machine, new OpcName("MethodC"), new Func<double, double, double>(MethodC));
+            methodD = new OpcMethodNode(machine, new OpcName("MethodD"), new Func<double, double, double>(MethodD));
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", machine))
+            {
+                server.Start();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Server started!");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                while (true)
+                {
+                    for (int i = 0; i < Called.Count; i++)
+                    {
+                        if (Called[i].Value == true)
+                        {
+                            Calls[i].Value++;
+                            Called[i].Value = false;
+                            Console.WriteLine($"Calls Method: {Calls[i].Value}");
+                        }
+                    }
+
+                    foreach (var item in Calls)
+                    {
+                        item.Status.Update(OpcStatusCode.Good);
+                        item.Timestamp = DateTime.UtcNow;
+                        item.Value=item.Value;
+                        item.ApplyChanges(server.SystemContext);
+                    }
+
+                    //random.Status.Update(OpcStatusCode.Good);
+                    //random.Timestamp = DateTime.UtcNow;
+                    //random.Value = rnd.Next();
+                    //random.ApplyChanges(server.SystemContext);
+                    //Console.WriteLine("Alive");
+                    //Thread.Sleep(1000);
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+
